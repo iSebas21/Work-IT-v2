ac006ec7fe1aa46d1ea2f32da146f625e30aeb42
Sebastian Manzo <75702202+iSebas21@users.noreply.github.com>
2021-11-20 19:15:38 -0600

    Add files via upload

--- database.sql
+++ database.sql
@@ -1,3 +1,32 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 21-11-2021 a las 02:13:58
+-- Versión del servidor: 10.4.21-MariaDB
+-- Versión de PHP: 8.0.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `work_it`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `admin`
+--
+
 CREATE TABLE `admin` (
   `admin_id` int(11) NOT NULL,
   `admin_name` varchar(100) NOT NULL,
@@ -11,7 +40,14 @@ CREATE TABLE `employees` (
   `employee_mail` varchar(50) NOT NULL,
   `employee_password` varchar(255) NOT NULL,
   `employee_admin` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4; 
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+CREATE TABLE `msg` (
+  `msg_id` int(11) NOT NULL,
+  `msg_origin` varchar(30) NOT NULL,
+  `msg_destination` varchar(30) NOT NULL,
+  `msg_text` varchar(300) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 CREATE TABLE `projects` (
   `pro_id` int(11) NOT NULL,
@@ -24,22 +60,59 @@ CREATE TABLE `projects` (
   `pro_file` mediumblob DEFAULT NULL,
   `pro_msg` varchar(300) DEFAULT ''
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
+--
+-- Indices de la tabla `admin`
+--
 ALTER TABLE `admin`
   ADD PRIMARY KEY (`admin_id`);
 
+--
+-- Indices de la tabla `employees`
+--
 ALTER TABLE `employees`
   ADD PRIMARY KEY (`employee_id`);
 
+--
+-- Indices de la tabla `msg`
+--
+ALTER TABLE `msg`
+  ADD PRIMARY KEY (`msg_id`);
+
+--
+-- Indices de la tabla `projects`
+--
 ALTER TABLE `projects`
   ADD PRIMARY KEY (`pro_id`);
 
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `admin`
+--
 ALTER TABLE `admin`
   MODIFY `admin_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
+--
+-- AUTO_INCREMENT de la tabla `employees`
+--
 ALTER TABLE `employees`
   MODIFY `employee_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
+--
+-- AUTO_INCREMENT de la tabla `msg`
+--
+ALTER TABLE `msg`
+  MODIFY `msg_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT de la tabla `projects`
+--
 ALTER TABLE `projects`
   MODIFY `pro_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
 COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
